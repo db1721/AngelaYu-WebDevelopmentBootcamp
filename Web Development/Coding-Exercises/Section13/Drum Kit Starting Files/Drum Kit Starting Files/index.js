be1e1d6b48f74d176bc5a2784830f1ed7bcbfb60
Dan Beck <63600571+db1721@users.noreply.github.com>
2022-03-22 21:12:16 -0400

    20220322

--- Web Development/Coding-Exercises/Section13/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Web Development/Coding-Exercises/Section13/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -3,9 +3,9 @@ var numerOfDrawButtons = document.querySelectorAll(".drum").length
 for (var i = 0; i < numerOfDrawButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
 
-    function handleClick() {
-        alert("I got clicked")
-        var audio = new Audio('audio_file.mp3');
-        audio.play();
-    }
+    this.style.color = "white";
+
 }
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//             audio.play();
